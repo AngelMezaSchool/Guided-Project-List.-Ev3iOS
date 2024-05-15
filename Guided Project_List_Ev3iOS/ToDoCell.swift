c1ed12df324727feb0ae3dad6d8368dd43a2e431
AngelMezaSchool <al03018750@tecmilenio.mx>
2024-05-14 21:55:03 -0600

    no funciona :(

---
+++ Guided Project_List_Ev3iOS/ToDoCell.swift
@@ -0,0 +1,23 @@
+//
+//  ToDoCell.swift
+//  Guided Project_List_Ev3iOS
+//
+//  Created by Angel Meza on 14/05/24.
+//
+
+import UIKit
+
+class ToDoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
